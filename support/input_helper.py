517abbbea8c3ee6f3ccebf4d5dd906100ad4cb3f
Julien Marrec <julien.marrec@gmail.com>
2020-10-16 15:53:07 +0200

    Exemple de comment mettre des fonctions de support dans un fichier .py et les appeler depuis un notebook

--- support/input_helper.py
+++ support/input_helper.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 ROOT_DIR = "RawData"
 
-def get_data(filename, dir = ROOT_DIR, force_download = False):
+def get_data(filename, directory=ROOT_DIR, force_download=False):
     """Download Excel dataset
-    
+
     Parameters
     ----------
     filename: string (optional)
@@ -14,12 +14,11 @@ def get_data(filename, dir = ROOT_DIR, force_download = False):
         Root_dir, location of the data
     force_download: bool (optional)
         if True, force redownload of data
-    
+
     Returns
     -------
     data: pandas.DataFrame
     """
-    
     if force_download or not os.path.exists(filename):
-        data = pd.ExcelFile(os.path.join(dir, filename))
+        data = pd.ExcelFile(os.path.join(directory, filename))
     return data
